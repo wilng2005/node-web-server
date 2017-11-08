37a7e3de7b77bdc8737721e37b347cf454e8bf0d
Wil Ng <wil.ng.2005@gmail.com>
2017-11-08 08:35:55 +0800

    Implemented projects page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/about',(req, res)=>{
 	});
 });
 
+app.get('/projects',(req, res)=>{
+	res.render('projects.hbs',{
+		pageTitle:"Projects page"
+	});
+});
+
 app.get('/bad',(req,res)=>{
 	res.send({
 		errorMessage:"An error has occurred. Again."
